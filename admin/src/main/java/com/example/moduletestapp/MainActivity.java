20fc669d76b70ab6a10c8978f1290814556d56d3
Sandeep Kushwaha <sandeepkush327@gmail.com>
2019-09-01 11:39:56 +0530

    admin using doctors profile layout

--- admin/src/main/java/com/example/moduletestapp/MainActivity.java
+++ admin/src/main/java/com/example/moduletestapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.moduletestapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "result: "+ SayHello.sayHello(), Toast.LENGTH_LONG).show();
+
+        final EditText editText = findViewById(R.id.doctor_profile_name);
+        Button button = findViewById(R.id.button_show_doctor_name);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "You Just Entered: " + editText.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
